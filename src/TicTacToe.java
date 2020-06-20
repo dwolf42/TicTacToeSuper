a9a6f72b0ed6e618b686c6505db30c91a1f20a51
lynx42 <bjarnhildar@gmail.com>
2020-06-20 23:18:37 +0200

    - created developer branch - changed checkAndPrint(); void method to checkSlots(); boolean method. This method should only validate if there are still slots available to be occupied by a player. - moved while-loop from former checkAndPrint(); method to main method. Since the game is basically a big while-loop I will stick to the loop for now. Should be adding some tales to it. - moved 2d array and related for-loop to print the array to a separate void method printPlayingField(); for now. Not quiet sure yet how this will work out later. Maybe I'll have to make a major change to this again. - renamed void method defaultOutput(); to playTurnMessage(); for better a understanding what it actually is for. - implemented void method for game over screen.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -35,8 +35,11 @@ public class TicTacToe {
 
     public static void main(String[] args) {
 
-        checkAndPrint();
-        defaultOutput();
+        while (checkSots()) {
+            printPlayingfield();
+            playTurnMessage();
+        }
+
         boolean flag = getAndValidatePlayerInput();
 
 
@@ -45,15 +48,22 @@ public class TicTacToe {
     } // main
 
 
-    public static void checkAndPrint() {
+    public static boolean checkSots() {
         /* The game runs in a while-loop, that is constantly checking if all slots are occupied,
            this means if the underscores have been replaced either by an X or O.
            As soon as all moves are made the game ends.
         */
-        while ((a1 == '_') || (a2 == '_') || (a3 == '_')
+
+        if ((a1 == '_') || (a2 == '_') || (a3 == '_')
                 || (b1 == '_') || (b2 == '_') || (b3 == '_')
                 || (c1 == '_') || (c2 == '_') || (c3 == '_')) {
+            return true;
+        }
+        return false;
+    }
 
+
+    public static void printPlayingfield() {
             /* This 2 dimensional array stores the basic look of the playing field, as well as the locations
                where moves can be placed and also stores the actual move as long as the game's running.
 
@@ -63,35 +73,33 @@ public class TicTacToe {
                C| _ _ _ |
                ◟--------◞
              */
-            char[][] ticSuper = {{'\u25DC', '-', '-', '1', '-', '2', '-', '3', '-', '\u25DD'},
-                    {'A', '|', ' ', a1, ' ', a2, ' ', a3, ' ', '|'},
-                    {'B', '|', ' ', b1, ' ', b2, ' ', b3, ' ', '|'},
-                    {'C', '|', ' ', c1, ' ', c2, ' ', c3, ' ', '|'},
-                    {'\u25DF', '-', '-', '-', '-', '-', '-', '-', '-', '\u25DE'},
-            };
+        char[][] ticSuper = {{'\u25DC', '-', '-', '1', '-', '2', '-', '3', '-', '\u25DD'},
+                {'A', '|', ' ', a1, ' ', a2, ' ', a3, ' ', '|'},
+                {'B', '|', ' ', b1, ' ', b2, ' ', b3, ' ', '|'},
+                {'C', '|', ' ', c1, ' ', c2, ' ', c3, ' ', '|'},
+                {'\u25DF', '-', '-', '-', '-', '-', '-', '-', '-', '\u25DE'},
+        };
 
-            // This for-loop prints the whole playing field before every turn.
-            for (int row = 0; row < 5; row++) {
-                int column;
-                for (column = 0; column < 9; column++) {
-                    System.out.print(ticSuper[row][column]);
-                }
-                System.out.print(ticSuper[row][column] + "\n");
+        // This for-loop prints the whole playing field before every turn.
+        for (int row = 0; row < 5; row++) {
+            int column;
+            for (column = 0; column < 9; column++) {
+                System.out.print(ticSuper[row][column]);
             }
-
-            break;
+            System.out.print(ticSuper[row][column] + "\n");
         }
+    }
 
-    } // checkAndPrint
 
-    public static void defaultOutput() {
+
+    public static void playTurnMessage() {
         // How to play, call player whose turn it is and ask for input
         System.out.println("\nValid formats: A1 X or b3 o");
         System.out.println("It's player " + player + "'s turn!");
         System.out.println("Enter: ");
 
 
-    } // defaultOutput
+    }
 
     public static boolean getAndValidatePlayerInput() {
         playerInput = scanner.nextLine();
@@ -105,5 +113,12 @@ public class TicTacToe {
 
    // public static void wrongInput() {
     //}
+
+    public static void gameOver() {
+        // After game finishes / all slots occupied
+        System.out.println("--------------");
+        System.out.println("Game Over! \ud83d\ude38");
+        System.out.println("--------------");
+    }
 
 } //class
