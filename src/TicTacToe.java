cb5d7123b2400d400eb7ae62a2397e4f4f121296
lynx42 <bjarnhildar@gmail.com>
2020-06-28 12:32:09 +0200

    test update

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -154,5 +154,6 @@ public class TicTacToe {
         System.out.println("\n--------------");
         System.out.println("Game Over! \ud83d\ude38");
         System.out.println("--------------");
+
     }
 }
